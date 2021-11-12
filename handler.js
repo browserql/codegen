9bdac169fdd9781fa3a3952f2784cf20655cc491
pierrot <pierre@drouillet.es>
2021-11-12 12:15:38 +0100

    Add handler

---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { parse, buildSchema, buildASTSchema } = require('graphql');
+const { join } = require('path');
+
+const [, , file, doc] = process.argv;
+
+const { handler } = require(join(process.cwd(), file));
+
+async function run() {
+  return await handler({
+    source: doc,
+    document: parse(doc),
+    schema: buildSchema(doc),
+    // ast: buildASTSchema(doc),
+  });
+}
+
+run()
+  .then(
+    // (output) => `======= codegen =======\n${output}\n======= codegen =======\n`
+    (output) =>
+      console.log(
+        `======= codegen =======\n${output}\n======= codegen =======\n`
+      )
+  )
+  .catch((error) => {
+    console.log('FAILED');
+    console.log(error);
+  });
