d9873a777a0a608e145fcd1bf606e465c90ee0ab
pierrot <pierre@drouillet.es>
2021-11-12 12:24:53 +0100

    Make executable

---
+++ bin.js
@@ -0,0 +1,276 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var child_process_1 = require("child_process");
+var promises_1 = require("fs/promises");
+var path_1 = require("path");
+var util_1 = require("util");
+function getConfigFile(configFile) {
+    return __awaiter(this, void 0, void 0, function () {
+        var stats, source, json, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, (0, promises_1.stat)(configFile)];
+                case 1:
+                    stats = _a.sent();
+                    if (!stats.isFile()) {
+                        throw new Error("Config file \"" + configFile + "\" is not a file");
+                    }
+                    return [4 /*yield*/, (0, promises_1.readFile)(configFile)];
+                case 2:
+                    source = _a.sent();
+                    json = void 0;
+                    try {
+                        json = JSON.parse(source.toString());
+                    }
+                    catch (error) {
+                        throw new Error("Config file \"" + configFile + "\" is not a valid JSON file");
+                    }
+                    return [2 /*return*/, json];
+                case 3:
+                    error_1 = _a.sent();
+                    throw new Error('Can not find config file');
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+function findGraphqlFiles(dir) {
+    return __awaiter(this, void 0, void 0, function () {
+        var files, graphqlFiles;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, (0, promises_1.readdir)(dir)];
+                case 1:
+                    files = _a.sent();
+                    graphqlFiles = [];
+                    return [4 /*yield*/, Promise.all(files.map(function (file) { return __awaiter(_this, void 0, void 0, function () {
+                            var stats, results;
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0:
+                                        if (!/\.graphql$/.test(file)) return [3 /*break*/, 1];
+                                        graphqlFiles.push((0, path_1.join)(dir, file));
+                                        return [3 /*break*/, 4];
+                                    case 1: return [4 /*yield*/, (0, promises_1.stat)((0, path_1.join)(dir, file))];
+                                    case 2:
+                                        stats = _a.sent();
+                                        if (!stats.isDirectory()) return [3 /*break*/, 4];
+                                        return [4 /*yield*/, findGraphqlFiles((0, path_1.join)(dir, file))];
+                                    case 3:
+                                        results = _a.sent();
+                                        graphqlFiles.push.apply(graphqlFiles, results);
+                                        _a.label = 4;
+                                    case 4: return [2 /*return*/];
+                                }
+                            });
+                        }); }))];
+                case 2:
+                    _a.sent();
+                    return [2 /*return*/, graphqlFiles];
+            }
+        });
+    });
+}
+function getSchema(sources) {
+    return __awaiter(this, void 0, void 0, function () {
+        var strings;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, Promise.all(sources.map(function (source) { return __awaiter(_this, void 0, void 0, function () {
+                        var fileName, stats, files, sources_1, string;
+                        var _this = this;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0:
+                                    fileName = (0, path_1.join)(process.cwd(), source);
+                                    return [4 /*yield*/, (0, promises_1.stat)(fileName)];
+                                case 1:
+                                    stats = _a.sent();
+                                    if (!stats.isDirectory()) return [3 /*break*/, 4];
+                                    return [4 /*yield*/, findGraphqlFiles(fileName)];
+                                case 2:
+                                    files = _a.sent();
+                                    return [4 /*yield*/, Promise.all(files.map(function (file) { return __awaiter(_this, void 0, void 0, function () {
+                                            var src;
+                                            return __generator(this, function (_a) {
+                                                switch (_a.label) {
+                                                    case 0:
+                                                        console.log();
+                                                        console.log('Reading GraphQL file', file);
+                                                        console.log();
+                                                        return [4 /*yield*/, (0, promises_1.readFile)(file)];
+                                                    case 1:
+                                                        src = _a.sent();
+                                                        return [2 /*return*/, src.toString()];
+                                                }
+                                            });
+                                        }); }))];
+                                case 3:
+                                    sources_1 = _a.sent();
+                                    return [2 /*return*/, sources_1.join('\n')];
+                                case 4:
+                                    console.log();
+                                    console.log('Reading GraphQL file', fileName);
+                                    console.log();
+                                    return [4 /*yield*/, (0, promises_1.readFile)((0, path_1.join)(process.cwd(), source))];
+                                case 5:
+                                    string = _a.sent();
+                                    return [2 /*return*/, string.toString()];
+                            }
+                        });
+                    }); }))];
+                case 1:
+                    strings = _a.sent();
+                    return [2 /*return*/, strings.join('\n')];
+            }
+        });
+    });
+}
+function codegen(configFile) {
+    if (configFile === void 0) { configFile = (0, path_1.join)(process.cwd(), 'codegen.json'); }
+    return __awaiter(this, void 0, void 0, function () {
+        var config, schema, generates, afterAll, schemas, graphqlSchema, _loop_1, _i, generates_1, generate;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, getConfigFile(configFile)];
+                case 1:
+                    config = _a.sent();
+                    schema = config.schema, generates = config.generates, afterAll = config.afterAll;
+                    schemas = Array.isArray(schema) ? schema : [schema];
+                    return [4 /*yield*/, getSchema(schemas)];
+                case 2:
+                    graphqlSchema = _a.sent();
+                    _loop_1 = function (generate) {
+                        var file, handler, _b, executable, output, _c, contents, posts;
+                        return __generator(this, function (_d) {
+                            switch (_d.label) {
+                                case 0:
+                                    file = generate.file, handler = generate.handler, _b = generate.executable, executable = _b === void 0 ? 'node' : _b;
+                                    console.log();
+                                    console.log('Generating', file);
+                                    console.log();
+                                    return [4 /*yield*/, new Promise(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+                                            var out, err, _a, exec, execs, ps;
+                                            return __generator(this, function (_b) {
+                                                out = [];
+                                                err = [];
+                                                _a = executable.split(/\s+/), exec = _a[0], execs = _a.slice(1);
+                                                ps = (0, child_process_1.spawn)(exec, __spreadArray(__spreadArray([], execs, true), [
+                                                    'node_modules/.bin/@browserql/codegen/handler.js',
+                                                    handler,
+                                                    graphqlSchema,
+                                                ], false));
+                                                ps.on('error', reject);
+                                                ps.on('close', function (status) {
+                                                    if (status === 0) {
+                                                        out.shift();
+                                                        resolve(out.join('\n'));
+                                                    }
+                                                    else {
+                                                        reject(new Error("Got unexpected status " + status + ": " + err.join('\n')));
+                                                    }
+                                                });
+                                                ps.stdout.on('data', function (data) {
+                                                    out.push(data.toString());
+                                                });
+                                                ps.stderr.on('data', function (data) {
+                                                    err.push(data.toString());
+                                                });
+                                                return [2 /*return*/];
+                                            });
+                                        }); })];
+                                case 1:
+                                    output = _d.sent();
+                                    _c = output.split('======= codegen ======='), contents = _c[1];
+                                    return [4 /*yield*/, (0, promises_1.writeFile)((0, path_1.join)(process.cwd(), file), contents)];
+                                case 2:
+                                    _d.sent();
+                                    if (!afterAll) return [3 /*break*/, 4];
+                                    posts = Array.isArray(afterAll) ? afterAll : [afterAll];
+                                    return [4 /*yield*/, Promise.all(posts.map(function (post) { return __awaiter(_this, void 0, void 0, function () {
+                                            return __generator(this, function (_a) {
+                                                switch (_a.label) {
+                                                    case 0: return [4 /*yield*/, (0, util_1.promisify)(child_process_1.exec)((0, path_1.join)(process.cwd(), post) + " " + (0, path_1.join)(process.cwd(), file))];
+                                                    case 1:
+                                                        _a.sent();
+                                                        return [2 /*return*/];
+                                                }
+                                            });
+                                        }); }))];
+                                case 3:
+                                    _d.sent();
+                                    _d.label = 4;
+                                case 4: return [2 /*return*/];
+                            }
+                        });
+                    };
+                    _i = 0, generates_1 = generates;
+                    _a.label = 3;
+                case 3:
+                    if (!(_i < generates_1.length)) return [3 /*break*/, 6];
+                    generate = generates_1[_i];
+                    return [5 /*yield**/, _loop_1(generate)];
+                case 4:
+                    _a.sent();
+                    _a.label = 5;
+                case 5:
+                    _i++;
+                    return [3 /*break*/, 3];
+                case 6: return [2 /*return*/];
+            }
+        });
+    });
+}
+var _a = process.argv, _configFile = _a[2];
+codegen(_configFile);
+// codegen configFile=codegen.json
